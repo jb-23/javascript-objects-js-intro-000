ba41cd42c708ce772742ae7f3ed0e52d2e3aacf9
Jason Bamford <scarfandgoggles@yahoo.co.uk>
2019-02-22 22:30:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ playlist['Elvis Costello'] = 'Radio Radio';
 function updatePlaylist(pl, artist, song) {
   pl[artist] = song;
 }
+
+function removeFromPlaylist(pl, artist) {
+  delete pl[artist];
+}
